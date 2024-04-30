0e469ec76a79a0e78eb349cd1701ab1fad32a028
Ivan Spiljak <ispiljak@gmail.com>
2024-04-30 18:51:42 +0200

    Updated webpack structure and created home page

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,49 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlBundlerPlugin = require("html-bundler-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: {
+    // define HTML files here (the key is output file w/o ext)
+    index: "./src/template.html", // => dist/index.html
+  },
   output: {
-    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  devtool: "inline-source-map",
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      title: "Wirtshaus zum Staudenhäusl",
-      inject: "head",
-      scriptLoading: "defer",
-    }),
-  ],
   module: {
     rules: [
+      // Note: enable processing of HTML files from entry
+      {
+        test: /\.html$/,
+        loader: HtmlBundlerPlugin.loader, // HTML loader
+      },
+
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        // Note: this plugin extracts CSS self, no style-loader needed
+        use: ["css-loader"],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
         type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
+        generator: {
+          // Note: define output filename of images
+          filename: "assets/img/[name].[hash:8][ext]",
+        },
       },
     ],
   },
+  plugins: [
+    new HtmlBundlerPlugin({
+      js: {
+        // output filename of extracted JS from source script loaded in HTML via `<script>` tag
+        filename: "js/[name].[contenthash:8].js",
+      },
+      css: {
+        // output filename of extracted CSS from source style loaded in HTML via `<link>` tag
+        filename: "css/[name].[contenthash:8].css",
+      },
+    }),
+  ],
 };
